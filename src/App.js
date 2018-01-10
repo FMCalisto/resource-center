57c736952368088fa892c0b3237bc4189242bea9
FMCalisto <FMCalisto@users.noreply.github.com>
2018-01-10 21:49:29 +0000

    [ADD] On Left Icon Button Touch Tap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
+
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+
 import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
   render() {
     return (
       <div className="App">
         <AppBar
-          title="Title"
+          title="MyApp"
           iconClassNameRight="muidocs-icon-navigation-expand-more"
+          onLeftIconButtonClick={() => this.setState({open: !this.state.open})}
         />
+        <Drawer open={this.state.open}>
+          <MenuItem>Menu Item</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+        </Drawer>
       </div>
     );
   }
